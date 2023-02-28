44bedf8d37adbbaabde4254e4523b938a70db6b6
AleksandrApoltsev <docapoltsev@yandex.ru>
2023-02-28 23:30:18 +0400

    start v2

---
+++ src/test/java/vorobey_english_ui/test_base/TestBase.java
@@ -0,0 +1,36 @@
+package vorobey_english_ui.test_base;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+
+
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import vorobey_english_ui.attach.Attach;
+
+public class TestBase {
+
+    @BeforeAll
+    static void configure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl = "http://vorobeyenglish.ru";
+        Configuration.browserSize = "1920x1080";
+       // Configuration.remote = "https://@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+}
